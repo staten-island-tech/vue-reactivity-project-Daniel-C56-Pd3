ea8ff8b29da25964d4b074e9e838963b13ee2cda
Daniel <danield494@nycstudents.net>
2026-02-06 14:00:58 -0500

    1

--- VueReact/src/router/index.js
+++ VueReact/src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import PizzaCreator from '@/Vues/Pizzacreator.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: PizzaCreator
+    }
+  ],
 })
 
 export default router
