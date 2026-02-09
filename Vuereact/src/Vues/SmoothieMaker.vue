c0136de4620e51faf6a834746ba70e8a29337346
Daniel <danield494@nycstudents.net>
2026-02-09 14:02:02 -0500

    1

---
+++ Vuereact/src/Vues/SmoothieMaker.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <smoothiecard v-for="value in source"/>
+        <smoothiecup :ingredients="ingredients"/>
+        <total :ingredients/>>
+    </div>
+</template>
+
+<script setup>
+    import {ref} from 'vue'
+    const ingredients = ref([
+        { },
+
+    ])
+</script>
+
+<style lang="scss" scoped>
+
+</style>
