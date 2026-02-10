317469c72538670b6fdb56c18dba09266eb0c7e3
Daniel <danield494@nycstudents.net>
2026-02-10 14:01:47 -0500

    1

--- Vuereact/src/Vues/SmoothieMaker.vue
+++ Vuereact/src/Vues/SmoothieMaker.vue
@@ -7,11 +7,34 @@
 </template>
 
 <script setup>
-    import {ref} from 'vue'
-    const ingredients = ref([
-        { },
+import { ref } from 'vue'
 
-    ])
+const ingredients = ref([
+  {
+    name: 'Banana',
+    quantity: 1,
+    calories: 105,
+    price: 0.50
+  },
+  {
+    name: 'Strawberries',
+    quantity: 150,
+    calories: 48,
+    price: 2.00
+  },
+  {
+    name: 'Almond Milk',
+    quantity: 250,
+    calories: 30,
+    price: 1.20
+  },
+  {
+    name: 'Honey',
+    quantity: 1,
+    calories: 64,
+    price: 0.80
+  }
+])
 </script>
 
 <style lang="scss" scoped>
